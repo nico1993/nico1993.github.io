54671d2ea16b030b559de30f7ba1aaf4d4c17cb6
Nicolás Doná <nico.01.06.93@gmail.com>
2019-03-12 16:02:08 -0300

    Primer commit

---
+++ precache-manifest.cbbb2fe912c49fd3066e8320792ccf1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59d59255f8eb97a9046b",
+    "url": "/static/css/main.e2f727b8.chunk.css"
+  },
+  {
+    "revision": "59d59255f8eb97a9046b",
+    "url": "/static/js/main.52df8be5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "4047826dbec2e405d188458d7d9fe21c",
+    "url": "/index.html"
+  }
+];
